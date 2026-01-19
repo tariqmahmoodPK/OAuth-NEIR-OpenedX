66543359009943605ded9d3a029c8ef14d45439a
Tariq Mahmood <75470442+tariqmahmoodPK@users.noreply.github.com>
2026-01-19 12:00:07 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="neir-auth",
-    version="0.1.11",
+    version="0.1.12",
     packages=find_packages(),
     include_package_data=True,
     entry_points={
