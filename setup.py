faa2bfd366f4810bb4e9129c5ab16512e986bdbd
Tariq Mahmood <75470442+tariqmahmoodPK@users.noreply.github.com>
2026-01-29 21:46:12 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="neir-auth",
-    version="0.1.14",
+    version="0.2.0",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=[
+        # Open edX already provides these, but declaring them makes the package self-describing.
+        "social-auth-core>=4.0.0",
+        "social-auth-app-django>=5.0.0",
+    ],
     entry_points={
         "openedx.core.djangoapps": [
             "neir_auth = neir_auth",
